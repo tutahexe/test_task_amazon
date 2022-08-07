2bf0766a7acc28fc5a7dbafc3ae3b39a2d7905d9
Mike <againstthesilence@gmail.com>
2022-08-07 15:41:04 +0300

    test_b fixes

--- tests/test_b.py
+++ tests/test_b.py
@@ -4,15 +4,19 @@ import pytest
 @pytest.mark.db
 def test_b_1(db):
     medium_rate = db.get_medium_rate()
-    assert medium_rate > 3.5
+    assert medium_rate > 3.5, f"Medium rate is {medium_rate}"
+
 
 @pytest.mark.db
 def test_b_2(db):
-    title = "kek"
+    title = "Modern CMake"
     search_results = db.get_items_with_title(title)
-    assert len(search_results) > 0
+    assert len(search_results) > 0, f"{title} not present in DB"
+
 
 @pytest.mark.db
 def test_b_3(db):
+    expected_name = "Accelerate DevOps"
     item = db.get_newest_item()
-    assert item.get_name() == "Accelerate DevOps"
+    first_item_name = item.get_name()
+    assert first_item_name == expected_name, f"First item is {first_item_name} while was expected  {expected_name}"
